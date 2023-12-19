fb6953bfc95b6f48236c35200719457c1176d79a
alindsharmasimply <alindsharmasimply@gmail.com>
2023-12-19 20:53:57 +0530

    Changes to Navbar

---
+++ app/NavBar.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { AiFillBug } from 'react-icons/ai';
+
+const NavBar = () => {
+  const currentPath = usePathname();
+  const links = [
+    { label: 'Dashboard', href: '/' },
+    { label: 'Issues', href: '/issues' },
+  ];
+  return (
+    <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
+      <Link href="/">
+        <AiFillBug />
+      </Link>
+      <ul className="flex space-x-6">
+        {links.map((link) => (
+          <li key={link.href}>
+            <Link
+              className={`${
+                link.href === currentPath ? 'text-zinc-950' : 'text-zinc-500'
+              } hover:text-zinc-950 transition-colors font-bold font-mono`}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
