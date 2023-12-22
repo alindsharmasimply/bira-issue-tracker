33f4e10a99605047b7613516379c4ad1bb186aa6
alindsharmasimply <alindsharmasimply@gmail.com>
2023-12-22 13:57:42 +0530

    Loading Skeleton added

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,7 +1,17 @@
+import { Box } from '@radix-ui/themes';
 import React from 'react';
+import Skeleton from 'react-loading-skeleton';
 
 const LoadingNewIssuePage = () => {
-  return <div>Loading...</div>;
+  return (
+    <div>
+      <Box className="max-w-xl">
+        Loading...
+        <Skeleton />
+        <Skeleton height="20rem" />
+      </Box>
+    </div>
+  );
 };
 
 export default LoadingNewIssuePage;
