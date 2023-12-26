a38ce82d09a905801b63580e3a6327ce390fb057
alindsharmasimply <alindsharmasimply@gmail.com>
2023-12-26 14:47:30 +0530

    Dashboard pages added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,35 @@
-export default function Home() {
-  return <div>Hello World</div>;
+import prisma from '@/prisma/client';
+import LatestIssues from './LatestIssues';
+import { Flex, Grid } from '@radix-ui/themes';
+import { Metadata } from 'next';
+import IssueSummary from './IssueSummary';
+import IssueChart from './IssueChart';
+
+export default async function Home() {
+  const open = await prisma.issue.count({
+    where: { status: 'OPEN' },
+  });
+  const inProgress = await prisma.issue.count({
+    where: { status: 'IN_PROGRESS' },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: 'CLOSED' },
+  });
+
+  return (
+    <Grid columns={{ initial: '1', md: '2' }} gap="5">
+      <Flex direction="column" gap="5">
+        <IssueSummary open={open} inProgress={inProgress} closed={closed} />
+        <IssueChart open={open} inProgress={inProgress} closed={closed} />
+      </Flex>
+      <LatestIssues />
+    </Grid>
+  );
 }
+
+export const dynamic = 'force-dynamic';
+
+export const metadata: Metadata = {
+  title: 'Issue Tracker - Dashboard',
+  description: 'View a summary of project issues',
+};
